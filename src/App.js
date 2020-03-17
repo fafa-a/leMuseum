67c56dae2cb1a7cc3832daef986cebf0910afe09
fab-le-long <fablelong@yahoo.fr>
2020-03-17 18:09:59 +0100

    first version background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./css/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Le Museum</h1>
-      </header>
+    <div className="app">
+      <header className="App-header">{/* <h1>Le Museum</h1> */}</header>
     </div>
   );
 }
