c136838288618a1df06de10b3acd532e1325a85c
fab-le-long <fablelong@yahoo.fr>
2020-03-22 15:37:30 +0100

    praying for no error

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class TheCarousel extends Component {
   render() {
@@ -9,39 +9,39 @@ class TheCarousel extends Component {
         <Carousel.Item>
           <img
             className="carouselImg d-block w-100"
-            src={require('../images/kaws1.jpg')}
+            src={require("../images/KAWS1.jpg")}
             alt="First slide"
           />
-        <Carousel.Caption>
-          {/* <h3>First slide label</h3>
+          <Carousel.Caption>
+            {/* <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="carouselImg d-block w-100"
-          src={require('../images/kaws2.jpg')}
-          alt="Second slide"
-        />
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="carouselImg d-block w-100"
+            src={require("../images/KAWS2.jpg")}
+            alt="Second slide"
+          />
 
-        <Carousel.Caption>
-          {/* <h3>Second slide label</h3>
+          <Carousel.Caption>
+            {/* <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="carouselImg d-block w-100"
-          src={require('../images/kaws3.jpg')}
-          alt="Third slide"
-        />
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="carouselImg d-block w-100"
+            src={require("../images/KAWS3.jpg")}
+            alt="Third slide"
+          />
 
-        <Carousel.Caption>
-          {/* <h3>Third slide label</h3>
+          <Carousel.Caption>
+            {/* <h3>Third slide label</h3>
           <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     );
   }
 }
